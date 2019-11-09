93846eafdf1f82b81fc8c77b3efdde438a42c116
jungeun960 <wjddms905@gmail.com>
2019-11-09 21:55:21 +0900

    recycleview 단어장 만들기 2 - 아이템 추가

--- app/src/main/java/com/example/ex/RecycleView2Activity.java
+++ app/src/main/java/com/example/ex/RecycleView2Activity.java
@@ -5,13 +5,16 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -68,19 +71,49 @@ public class RecycleView2Activity extends AppCompatActivity {
 
         Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
+            // 1. 화면 아래쪽에 있는 데이터 추가 버튼을 클릭하면
             @Override
             public void onClick(View v) {
+                // 2. 레이아웃 파일 edit_box.xml 을 불러와서 화면에 다이얼로그를 보여줍니다.
+                AlertDialog.Builder builder = new AlertDialog.Builder(RecycleView2Activity.this);
 
-                count++;
+                View view = LayoutInflater.from(RecycleView2Activity.this)
+                        .inflate(R.layout.edit_box, null, false);
+                builder.setView(view);
 
-                Dictionary data = new Dictionary(count+"","Apple" + count, "사과" + count);
 
-                //mArrayList.add(0, dict); //RecyclerView의 첫 줄에 삽입
-                mArrayList.add(data); // RecyclerView의 마지막 줄에 삽입
+                final Button ButtonSubmit = (Button) view.findViewById(R.id.button_dialog_submit);
+                final EditText editTextID = (EditText) view.findViewById(R.id.edittext_dialog_id);
+                final EditText editTextEnglish = (EditText) view.findViewById(R.id.edittext_dialog_endlish);
+                final EditText editTextKorean = (EditText) view.findViewById(R.id.edittext_dialog_korean);
 
-                mAdapter.notifyDataSetChanged();             }
+                ButtonSubmit.setText("삽입");
+
+                final AlertDialog dialog = builder.create();
+
+
+                // 3. 다이얼로그에 있는 삽입 버튼을 클릭하면
+                ButtonSubmit.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // 4. 사용자가 입력한 내용을 가져와서
+                        String strID = editTextID.getText().toString();
+                        String strEnglish = editTextEnglish.getText().toString();
+                        String strKorean = editTextKorean.getText().toString();
+
+                        // 5. ArrayList에 추가하고
+                        Dictionary dict = new Dictionary(strID, strEnglish, strKorean );
+                        mArrayList.add(0, dict); //첫번째 줄에 삽입됨
+                        //mArrayList.add(dict); //마지막 줄에 삽입됨
+
+                        // 6. 어댑터에서 RecyclerView에 반영하도록 합니다.
+                        mAdapter.notifyItemInserted(0);
+                        //mAdapter.notifyDataSetChanged();
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
         });
-
     }
     public interface ClickListener {
         void onClick(View view, int position);
